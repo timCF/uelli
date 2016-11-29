8b3df8d2e434a12ad77ec73baae4c0dfb3ec43db
tim <tim@iMac-tim.local>
2016-11-29 19:16:41 +0300

    fix try-catch macro to avoid compile-time warnings

--- lib/uelli.ex
+++ lib/uelli.ex
@@ -37,7 +37,6 @@ defmodule Uelli do
 				unquote(body)
 			catch
 				signal, error -> {:error, {{signal, error}, :erlang.get_stacktrace}}
-				error -> {:error, {error, :erlang.get_stacktrace}}
 			rescue
 				error -> {:error, {error, :erlang.get_stacktrace}}
 			end
